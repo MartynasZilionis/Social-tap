6af0f9b08a390c6f3a6db037b892e340e7138cdd
Paulius <pauliusval99@hotmail.com>
2017-09-18 19:47:26 +0300

    initialize project

---
+++ social-tap/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_tap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
